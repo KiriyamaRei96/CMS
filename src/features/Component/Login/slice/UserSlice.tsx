37e105b033b9453d6710771ed7e124c05ce5d8a5
Hoang-Fe <lamduchoa96@gmail.com>
2022-10-13 22:09:41 +0700

    ghep api

--- src/features/Component/Login/slice/UserSlice.tsx
+++ src/features/Component/Login/slice/UserSlice.tsx
@@ -1,14 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-interface UserInfo {
+// import clearCookie from "../../../function/clearCookie";
+import openNotificationWithIcon from "../../../function/toast";
+interface info {
   id?: number;
   firstname?: string;
   lastname?: string;
   email?: string;
   phone?: number | null;
   role?: string;
+}
+interface UserInfo {
+  info?: info;
   isLogin: boolean;
-  errMessage?: string;
 }
 
 const initialState: UserInfo = {
@@ -17,18 +20,43 @@ const initialState: UserInfo = {
 export const userInfoSlice = createSlice({
   name: "userInfo",
   initialState,
-  reducers: {},
+  reducers: {
+    reLogin: (state) => {
+      state.isLogin = true;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase("LOGIN-SUCCESS", (state, action: any) => {
-        document.cookie = `token=${action.payload}`;
-
-        console.log(document.cookie);
+        openNotificationWithIcon(
+          "success",
+          "Đăng nhập thành công",
+          "Chào mừng bạn đến với CMS"
+        );
         state.isLogin = true;
       })
       .addCase("LOGIN_FAILED", (state, action: any) => {
-        state.errMessage = action.message;
+        openNotificationWithIcon(
+          "error",
+          "Đăng nhập không thành công",
+          "Vui lòng kiểm tra lại thông tin"
+        );
+      })
+      .addCase("LOG_OUT", (state, action: any) => {
+        // document.cookie.split(";").forEach(function (c) {
+        //   document.cookie = c
+        //     .replace(/^ +/, "")
+        //     .replace(
+        //       /=.*/,
+        //       "=;expires=" + new Date().toUTCString() + ";path=/"
+        //     );
+        // });
+        return (state = { isLogin: false });
+      })
+      .addCase("GET_USER_INFO", (state, action: any) => {
+        state.info = action.payload;
       });
   },
 });
 export const userInfoSelector = (state) => state.userInfo;
+export const { reLogin } = userInfoSlice.actions;
