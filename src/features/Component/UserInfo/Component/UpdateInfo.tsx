17b6d9de963d7c07c28a50509026c1c17b52f1d7
Hoang-Fe <lamduchoa96@gmail.com>
2022-10-14 17:58:10 +0700

    thêm thông tin

--- src/features/Component/UserInfo/Component/UpdateInfo.tsx
+++ src/features/Component/UserInfo/Component/UpdateInfo.tsx
@@ -1,5 +1,5 @@
 import { Button, Form, Input } from "antd";
-import React, { memo } from "react";
+import React from "react";
 import clsx from "clsx";
 import style from "../style.module.scss";
 import { useDispatch } from "react-redux";
@@ -9,54 +9,53 @@ export interface UpdateInfoProps {}
 export function UpdateInfo(props: UpdateInfoProps) {
   const dispatch = useDispatch();
   const onFinish = (value: any) => {
-    console.log(value);
     dispatch({ type: "USER_UPDATE_INFO_REQUESTED", payload: value });
   };
   return (
     <div className={clsx(style.form)}>
-      <Form onFinish={onFinish} layout="vertical">
+      <Form onFinish={onFinish} layout='vertical'>
         <Form.Item
           style={{ margin: "10px" }}
           rules={[
             { required: true, message: "Không được bỏ trống trường này!" },
           ]}
-          name="firstname"
-          label="Họ"
+          name='firstname'
+          label='Họ'
         >
-          <Input type="text" />
+          <Input type='text' />
         </Form.Item>
         <Form.Item
           style={{ margin: "10px" }}
           rules={[
             { required: true, message: "Không được bỏ trống trường này!" },
           ]}
-          name="lastname"
-          label="Tên"
+          name='lastname'
+          label='Tên'
         >
-          <Input type="text" />
+          <Input type='text' />
         </Form.Item>
         <Form.Item
           style={{ margin: "10px" }}
           rules={[
             { required: true, message: "Không được bỏ trống trường này!" },
           ]}
-          name="email"
-          label="Email"
+          name='email'
+          label='Email'
         >
-          <Input type="text" />
+          <Input type='text' />
         </Form.Item>
         <Form.Item
           style={{ margin: "10px" }}
           rules={[
             { required: true, message: "Không được bỏ trống trường này!" },
           ]}
-          name="phone"
-          label="Số điện thoại"
+          name='phone'
+          label='Số điện thoại'
         >
-          <Input type="text" />
+          <Input type='text' />
         </Form.Item>
         <Form.Item style={{ margin: "10px" }}>
-          <Button htmlType="submit" type="primary">
+          <Button htmlType='submit' type='primary'>
             Xác Nhận
           </Button>
         </Form.Item>
