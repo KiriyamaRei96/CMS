d4116c3d2c18167289dc57f657cf0084cac05ef2
rei <lamduchoa96@gmail.com>
2022-11-19 21:56:20 +0700

    done

--- src/features/Component/ContentEditor/index.tsx
+++ src/features/Component/ContentEditor/index.tsx
@@ -1,18 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState, convertToRaw } from "draft-js";
+import {
+  EditorState,
+  convertToRaw,
+  ContentState,
+  convertFromHTML,
+} from "draft-js";
 import draftToHtml from "draftjs-to-html";
-export interface ContentEditorProps {}
+import htmlToDraft from "html-to-draftjs";
+import useDebounce from "../Debounce";
+import { call } from "redux-saga/effects";
+import { callApi } from "../../../Api/Axios";
+import getCookie from "../../../Api/getCookie";
+export interface ContentEditorProps {
+  content: any;
+  setContent: Function;
+}
 
-const ContentEditor = (props: ContentEditorProps) => {
-  const [text, setText] = useState<any>(EditorState.createEmpty());
+const ContentEditor = ({ content, setContent }: ContentEditorProps) => {
+  const html: any = htmlToDraft(content);
+  const [text, setText] = useState(
+    EditorState.createWithContent(ContentState.createFromBlockArray(html))
+  );
 
+  useEffect(() => {
+    setContent("content", draftToHtml(convertToRaw(text.getCurrentContent())));
+  }, [useDebounce(text, 500)]);
   return (
     <>
       <Editor
         editorState={text}
+        editorClassName="editor"
         onEditorStateChange={(e) => {
           setText(e);
+        }}
+        toolbar={{
+          image: {
+            previewImage: true,
+            uploadEnabled: true,
+            uploadCallback: async (e) => {
+              const formData = new FormData();
+              formData.append("file", e);
+              const cookie = getCookie("token");
+              const res = await callApi
+                .post("/v1/asset/upload", formData, {
+                  headers: { Authorization: cookie },
+                })
+                .then((response) => response.data)
+                .catch((err) => console.log(err));
+
+              if (res.status === 1) {
+                return new Promise((resolve, reject) => {
+                  resolve({
+                    data: { file: res.data.path, link: res.data.path },
+                  });
+                });
+              }
+
+              if (res.status === 0) {
+                return new Promise((resolve, reject) => {
+                  console.error("error", res.status);
+                  resolve({
+                    data: {
+                      link: res.errors,
+                    },
+                  });
+                });
+              }
+            },
+          },
         }}
       ></Editor>
     </>
